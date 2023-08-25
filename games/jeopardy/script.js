9ef0b66066513596b384a42ef2b9e55e0443b052
Cocaine-Potato <Cocaine-Potato@users.noreply.github.com>
2023-08-25 03:36:50 +0200

    Update script.js

--- games/jeopardy/script.js
+++ games/jeopardy/script.js
@@ -2,3 +2,11 @@ let questions;
 $.getJSON("https://cocaine-potato.github.io/media/files/json/questions.json").done(function( data ) {
     questions = data;
 });
+
+let m = Math;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+console.log(questions[getRndInteger(0, questions.length-1)].points)
