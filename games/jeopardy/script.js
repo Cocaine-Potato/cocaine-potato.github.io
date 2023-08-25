962d17e218e294b14353c1eff1c97fa1365aa010
Cocaine-Potato <Cocaine-Potato@users.noreply.github.com>
2023-08-25 04:12:14 +0200

    stuff

--- games/jeopardy/script.js
+++ games/jeopardy/script.js
@@ -1,9 +1,11 @@
+$.ajaxSetup({
+    async: false
+});
+
 let questions;
 $.getJSON("https://cocaine-potato.github.io/media/files/json/questions.json").done(function( data ) {
     questions = data;
 });
-
-let m = Math;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
